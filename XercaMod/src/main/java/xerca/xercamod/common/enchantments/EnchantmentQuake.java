f04f8aadc99fc522c8a4a38ed2612775019b0ef1
ercan <xercax@gmail.com>
2020-02-20 12:56:51 +0300

    Fixed enchantments getting applied to swords in the anvil. Upgraded forge version.

--- XercaMod/src/main/java/xerca/xercamod/common/enchantments/EnchantmentQuake.java
+++ XercaMod/src/main/java/xerca/xercamod/common/enchantments/EnchantmentQuake.java
@@ -62,6 +62,7 @@ public class EnchantmentQuake extends Enchantment {
      */
     @Override
     public boolean canApply(ItemStack stack) {
-        return stack.canApplyAtEnchantingTable(this);
+        //return stack.canApplyAtEnchantingTable(this);
+        return this.canApplyAtEnchantingTable(stack);
     }
 }
