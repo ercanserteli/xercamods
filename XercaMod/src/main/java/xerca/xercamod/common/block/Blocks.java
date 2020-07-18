fa3d096ef2cef1c59539dc45dfa2bf59c6921bab
ercan <xercax@gmail.com>
2020-07-18 14:05:38 +0300

    Added every colored terracotta tile variants Added stonecutting recipes for terracotta tiles Added a new creative tab for decoration blocks

--- XercaMod/src/main/java/xerca/xercamod/common/block/Blocks.java
+++ XercaMod/src/main/java/xerca/xercamod/common/block/Blocks.java
@@ -87,9 +87,59 @@ public class Blocks {
     public static final Block CARVED_SPRUCE_7 = null;
     public static final Block CARVED_SPRUCE_8 = null;
 
+    public static final BlockTerracottaTile BLACK_TERRATILE = null;
+    public static final BlockTerracottaTile BLUE_TERRATILE = null;
+    public static final BlockTerracottaTile BROWN_TERRATILE = null;
+    public static final BlockTerracottaTile CYAN_TERRATILE = null;
+    public static final BlockTerracottaTile GRAY_TERRATILE = null;
+    public static final BlockTerracottaTile GREEN_TERRATILE = null;
+    public static final BlockTerracottaTile LIGHT_BLUE_TERRATILE = null;
+    public static final BlockTerracottaTile LIGHT_GRAY_TERRATILE = null;
+    public static final BlockTerracottaTile LIME_TERRATILE = null;
+    public static final BlockTerracottaTile MAGENTA_TERRATILE = null;
     public static final BlockTerracottaTile ORANGE_TERRATILE = null;
+    public static final BlockTerracottaTile PINK_TERRATILE = null;
+    public static final BlockTerracottaTile PURPLE_TERRATILE = null;
+    public static final BlockTerracottaTile RED_TERRATILE = null;
+    public static final BlockTerracottaTile WHITE_TERRATILE = null;
+    public static final BlockTerracottaTile YELLOW_TERRATILE = null;
+    public static final BlockTerracottaTile TERRATILE = null;
+
+    public static final BlockTerracottaTileSlab BLACK_TERRATILE_SLAB = null;
+    public static final BlockTerracottaTileSlab BLUE_TERRATILE_SLAB = null;
+    public static final BlockTerracottaTileSlab BROWN_TERRATILE_SLAB = null;
+    public static final BlockTerracottaTileSlab CYAN_TERRATILE_SLAB = null;
+    public static final BlockTerracottaTileSlab GRAY_TERRATILE_SLAB = null;
+    public static final BlockTerracottaTileSlab GREEN_TERRATILE_SLAB = null;
+    public static final BlockTerracottaTileSlab LIGHT_BLUE_TERRATILE_SLAB = null;
+    public static final BlockTerracottaTileSlab LIGHT_GRAY_TERRATILE_SLAB = null;
+    public static final BlockTerracottaTileSlab LIME_TERRATILE_SLAB = null;
+    public static final BlockTerracottaTileSlab MAGENTA_TERRATILE_SLAB = null;
     public static final BlockTerracottaTileSlab ORANGE_TERRATILE_SLAB = null;
-    public static final StairsBlock ORANGE_TERRATILE_STAIRS = null;
+    public static final BlockTerracottaTileSlab PINK_TERRATILE_SLAB = null;
+    public static final BlockTerracottaTileSlab PURPLE_TERRATILE_SLAB = null;
+    public static final BlockTerracottaTileSlab RED_TERRATILE_SLAB = null;
+    public static final BlockTerracottaTileSlab WHITE_TERRATILE_SLAB = null;
+    public static final BlockTerracottaTileSlab YELLOW_TERRATILE_SLAB = null;
+    public static final BlockTerracottaTileSlab TERRATILE_SLAB = null;
+
+    public static final BlockTerracottaTileStairs BLACK_TERRATILE_STAIRS = null;
+    public static final BlockTerracottaTileStairs BLUE_TERRATILE_STAIRS = null;
+    public static final BlockTerracottaTileStairs BROWN_TERRATILE_STAIRS = null;
+    public static final BlockTerracottaTileStairs CYAN_TERRATILE_STAIRS = null;
+    public static final BlockTerracottaTileStairs GRAY_TERRATILE_STAIRS = null;
+    public static final BlockTerracottaTileStairs GREEN_TERRATILE_STAIRS = null;
+    public static final BlockTerracottaTileStairs LIGHT_BLUE_TERRATILE_STAIRS = null;
+    public static final BlockTerracottaTileStairs LIGHT_GRAY_TERRATILE_STAIRS = null;
+    public static final BlockTerracottaTileStairs LIME_TERRATILE_STAIRS = null;
+    public static final BlockTerracottaTileStairs MAGENTA_TERRATILE_STAIRS = null;
+    public static final BlockTerracottaTileStairs ORANGE_TERRATILE_STAIRS = null;
+    public static final BlockTerracottaTileStairs PINK_TERRATILE_STAIRS = null;
+    public static final BlockTerracottaTileStairs PURPLE_TERRATILE_STAIRS = null;
+    public static final BlockTerracottaTileStairs RED_TERRATILE_STAIRS = null;
+    public static final BlockTerracottaTileStairs WHITE_TERRATILE_STAIRS = null;
+    public static final BlockTerracottaTileStairs YELLOW_TERRATILE_STAIRS = null;
+    public static final BlockTerracottaTileStairs TERRATILE_STAIRS = null;
 
     @Mod.EventBusSubscriber(modid = XercaMod.MODID, bus=Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistrationHandler {
@@ -171,9 +221,59 @@ public class Blocks {
                     new BlockCarvedLog("carved_spruce_7"),
                     new BlockCarvedLog("carved_spruce_8"),
 
+                    new BlockTerracottaTile().setRegistryName("black_terratile"),
+                    new BlockTerracottaTile().setRegistryName("blue_terratile"),
+                    new BlockTerracottaTile().setRegistryName("brown_terratile"),
+                    new BlockTerracottaTile().setRegistryName("cyan_terratile"),
+                    new BlockTerracottaTile().setRegistryName("gray_terratile"),
+                    new BlockTerracottaTile().setRegistryName("green_terratile"),
+                    new BlockTerracottaTile().setRegistryName("light_blue_terratile"),
+                    new BlockTerracottaTile().setRegistryName("light_gray_terratile"),
+                    new BlockTerracottaTile().setRegistryName("lime_terratile"),
+                    new BlockTerracottaTile().setRegistryName("magenta_terratile"),
                     new BlockTerracottaTile().setRegistryName("orange_terratile"),
+                    new BlockTerracottaTile().setRegistryName("pink_terratile"),
+                    new BlockTerracottaTile().setRegistryName("purple_terratile"),
+                    new BlockTerracottaTile().setRegistryName("red_terratile"),
+                    new BlockTerracottaTile().setRegistryName("white_terratile"),
+                    new BlockTerracottaTile().setRegistryName("yellow_terratile"),
+                    new BlockTerracottaTile().setRegistryName("terratile"),
+
+                    new BlockTerracottaTileSlab().setRegistryName("black_terratile_slab"),
+                    new BlockTerracottaTileSlab().setRegistryName("blue_terratile_slab"),
+                    new BlockTerracottaTileSlab().setRegistryName("brown_terratile_slab"),
+                    new BlockTerracottaTileSlab().setRegistryName("cyan_terratile_slab"),
+                    new BlockTerracottaTileSlab().setRegistryName("gray_terratile_slab"),
+                    new BlockTerracottaTileSlab().setRegistryName("green_terratile_slab"),
+                    new BlockTerracottaTileSlab().setRegistryName("light_blue_terratile_slab"),
+                    new BlockTerracottaTileSlab().setRegistryName("light_gray_terratile_slab"),
+                    new BlockTerracottaTileSlab().setRegistryName("lime_terratile_slab"),
+                    new BlockTerracottaTileSlab().setRegistryName("magenta_terratile_slab"),
                     new BlockTerracottaTileSlab().setRegistryName("orange_terratile_slab"),
-                    new StairsBlock(() -> ORANGE_TERRATILE.getDefaultState(), Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5f).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("orange_terratile_stairs")
+                    new BlockTerracottaTileSlab().setRegistryName("pink_terratile_slab"),
+                    new BlockTerracottaTileSlab().setRegistryName("purple_terratile_slab"),
+                    new BlockTerracottaTileSlab().setRegistryName("red_terratile_slab"),
+                    new BlockTerracottaTileSlab().setRegistryName("white_terratile_slab"),
+                    new BlockTerracottaTileSlab().setRegistryName("yellow_terratile_slab"),
+                    new BlockTerracottaTileSlab().setRegistryName("terratile_slab"),
+
+                    new BlockTerracottaTileStairs(() -> BLACK_TERRATILE.getDefaultState()).setRegistryName("black_terratile_stairs"),
+                    new BlockTerracottaTileStairs(() -> BLUE_TERRATILE.getDefaultState()).setRegistryName("blue_terratile_stairs"),
+                    new BlockTerracottaTileStairs(() -> BROWN_TERRATILE.getDefaultState()).setRegistryName("brown_terratile_stairs"),
+                    new BlockTerracottaTileStairs(() -> CYAN_TERRATILE.getDefaultState()).setRegistryName("cyan_terratile_stairs"),
+                    new BlockTerracottaTileStairs(() -> GRAY_TERRATILE.getDefaultState()).setRegistryName("gray_terratile_stairs"),
+                    new BlockTerracottaTileStairs(() -> GREEN_TERRATILE.getDefaultState()).setRegistryName("green_terratile_stairs"),
+                    new BlockTerracottaTileStairs(() -> LIGHT_BLUE_TERRATILE.getDefaultState()).setRegistryName("light_blue_terratile_stairs"),
+                    new BlockTerracottaTileStairs(() -> LIGHT_GRAY_TERRATILE.getDefaultState()).setRegistryName("light_gray_terratile_stairs"),
+                    new BlockTerracottaTileStairs(() -> LIME_TERRATILE.getDefaultState()).setRegistryName("lime_terratile_stairs"),
+                    new BlockTerracottaTileStairs(() -> MAGENTA_TERRATILE.getDefaultState()).setRegistryName("magenta_terratile_stairs"),
+                    new BlockTerracottaTileStairs(() -> ORANGE_TERRATILE.getDefaultState()).setRegistryName("orange_terratile_stairs"),
+                    new BlockTerracottaTileStairs(() -> PINK_TERRATILE.getDefaultState()).setRegistryName("pink_terratile_stairs"),
+                    new BlockTerracottaTileStairs(() -> PURPLE_TERRATILE.getDefaultState()).setRegistryName("purple_terratile_stairs"),
+                    new BlockTerracottaTileStairs(() -> RED_TERRATILE.getDefaultState()).setRegistryName("red_terratile_stairs"),
+                    new BlockTerracottaTileStairs(() -> WHITE_TERRATILE.getDefaultState()).setRegistryName("white_terratile_stairs"),
+                    new BlockTerracottaTileStairs(() -> YELLOW_TERRATILE.getDefaultState()).setRegistryName("yellow_terratile_stairs"),
+                    new BlockTerracottaTileStairs(() -> TERRATILE.getDefaultState()).setRegistryName("terratile_stairs")
             );
         }
     }
