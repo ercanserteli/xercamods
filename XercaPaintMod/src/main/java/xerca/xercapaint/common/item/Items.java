981f8ad605464438b4087f5c38fb1ba7e56f072c
ercan <xercax@gmail.com>
2023-07-26 23:04:25 +0300

    Started the update to 1.20.x Update finished for paint mod Updated confetti sprites

--- XercaPaintMod/src/main/java/xerca/xercapaint/common/item/Items.java
+++ XercaPaintMod/src/main/java/xerca/xercapaint/common/item/Items.java
@@ -1,6 +1,11 @@
 package xerca.xercapaint.common.item;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.SimpleCraftingRecipeSerializer;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,15 +18,41 @@ import xerca.xercapaint.common.item.crafting.RecipeCraftPalette;
 import xerca.xercapaint.common.item.crafting.RecipeFillPalette;
 import xerca.xercapaint.common.item.crafting.RecipeTaglessShaped;
 
+import java.util.Arrays;
+
 public final class Items {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, XercaPaint.MODID);
+
     public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, XercaPaint.MODID);
+
     public static final RegistryObject<ItemPalette> ITEM_PALETTE = ITEMS.register("item_palette", ItemPalette::new);
     public static final RegistryObject<ItemCanvas> ITEM_CANVAS = ITEMS.register("item_canvas", () -> new ItemCanvas(CanvasType.SMALL));
     public static final RegistryObject<ItemCanvas> ITEM_CANVAS_LARGE = ITEMS.register("item_canvas_large", () -> new ItemCanvas(CanvasType.LARGE));
     public static final RegistryObject<ItemCanvas> ITEM_CANVAS_LONG = ITEMS.register("item_canvas_long", () -> new ItemCanvas(CanvasType.LONG));
     public static final RegistryObject<ItemCanvas> ITEM_CANVAS_TALL = ITEMS.register("item_canvas_tall", () -> new ItemCanvas(CanvasType.TALL));
     public static final RegistryObject<ItemEasel> ITEM_EASEL = ITEMS.register("item_easel", () -> new ItemEasel(new Item.Properties().stacksTo(1)));
+
+    public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, XercaPaint.MODID);
+    public static final RegistryObject<CreativeModeTab> PAINT_TAB = TABS.register("paint_tab", () ->
+            CreativeModeTab.builder()
+                    .title(Component.translatable("item_group." + XercaPaint.MODID + ".paint_tab"))
+                    .icon(() -> ITEM_PALETTE.get().getDefaultInstance())
+                    .displayItems((params, output) -> {
+                        ItemStack fullPalette = new ItemStack(ITEM_PALETTE.get());
+                        byte[] basicColors = new byte[16];
+                        Arrays.fill(basicColors, (byte)1);
+                        fullPalette.getOrCreateTag().putByteArray("basic", basicColors);
+
+                        output.accept(ITEM_PALETTE.get());
+                        output.accept(fullPalette);
+                        output.accept(ITEM_CANVAS.get());
+                        output.accept(ITEM_CANVAS_LONG.get());
+                        output.accept(ITEM_CANVAS_TALL.get());
+                        output.accept(ITEM_CANVAS_LARGE.get());
+                        output.accept(ITEM_EASEL.get());
+                    })
+                    .build()
+    );
 
     public static final RegistryObject<RecipeSerializer<RecipeCraftPalette>> CRAFTING_SPECIAL_PALETTE_CRAFTING = RECIPE_SERIALIZERS.register(
             "crafting_special_palette_crafting", () -> new SimpleCraftingRecipeSerializer<>(RecipeCraftPalette::new));
