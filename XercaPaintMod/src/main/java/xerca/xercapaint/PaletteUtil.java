5d73c1f28c86982b3639416ebf38920cb17891fe
Ercan Serteli <xercax@gmail.com>
2025-11-01 21:38:15 +0300

    Applied fix to the other instance

--- XercaPaintMod/src/main/java/xerca/xercapaint/PaletteUtil.java
+++ XercaPaintMod/src/main/java/xerca/xercapaint/PaletteUtil.java
@@ -50,7 +50,7 @@ public class PaletteUtil {
             int averageMaximum = (int)(Math.max(Math.max(a.r, a.g), a.b)*ratio) + (int)(Math.max(Math.max(b.r, b.g), b.b)*(1-ratio));
 
             int maximumOfAverage = Math.max(Math.max(res.r, res.g), res.b);
-            int gainFactor = averageMaximum / maximumOfAverage;
+            int gainFactor = maximumOfAverage == 0 ? 0 : averageMaximum / maximumOfAverage;
 
             res.r *= gainFactor;
             res.g *= gainFactor;
