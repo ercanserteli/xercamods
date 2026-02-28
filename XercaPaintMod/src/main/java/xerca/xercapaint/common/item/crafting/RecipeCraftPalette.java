d496516b692fcbf3c21a2e810151a3b6d6defd0e
ercan <xercax@gmail.com>
2026-02-28 17:01:15 +0300

    Made palette recipes more robust against unexpected input Added palette recipe tests

--- XercaPaintMod/src/main/java/xerca/xercapaint/common/item/crafting/RecipeCraftPalette.java
+++ XercaPaintMod/src/main/java/xerca/xercapaint/common/item/crafting/RecipeCraftPalette.java
@@ -5,6 +5,7 @@ import net.minecraft.core.NonNullList;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.DyeItem;
@@ -13,105 +14,136 @@ import net.minecraft.world.item.crafting.CraftingBookCategory;
 import net.minecraft.world.item.crafting.CustomRecipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
 import xerca.xercapaint.common.item.Items;
 
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.ArrayList;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class RecipeCraftPalette extends CustomRecipe {
-    private static final ResourceLocation plank = new ResourceLocation("minecraft:planks");
-
     public RecipeCraftPalette(ResourceLocation id, CraftingBookCategory category) {
         super(id, category);
     }
 
     private boolean isPlank(ItemStack stack){
-        return stack.getTags().anyMatch((p)-> p.location().equals(plank));
+        return !stack.isEmpty() && stack.is(ItemTags.PLANKS);
     }
 
-    private boolean isDye(ItemStack stack){
-        return stack.getItem() instanceof DyeItem;
-    }
-
-    private boolean isPlankRow(CraftingContainer inv, int row){
-        int plankCount = 0;
-        for(int j = 0; j < inv.getWidth(); ++j) {
-            int id = row*inv.getWidth() + j;
-            ItemStack stack = inv.getItem(id);
-            if(isPlank(stack)){
-                plankCount++;
-            }
+    private static int getBasicColorIndex(ItemStack stack) {
+        if (!(stack.getItem() instanceof DyeItem dyeItem)) {
+            return -1;
         }
-        return plankCount == 3;
+        DyeColor dyeColor = dyeItem.getDyeColor();
+        if (dyeColor == null || stack.getItem() != DyeItem.byColor(dyeColor)) {
+            return -1;
+        }
+        int colorId = dyeColor.getId();
+        if (colorId < 0 || colorId >= 16) {
+            return -1;
+        }
+        return 15 - colorId;
     }
 
-    private int findPlankRow(CraftingContainer inv){
-        for(int i = 0; i < inv.getHeight(); ++i) {
-            if(isPlankRow(inv, i)){
-                return i;
-            }
-        }
-        return -1;
+    public static boolean isDye(ItemStack stack) {
+        return getBasicColorIndex(stack) >= 0;
     }
 
     @Nullable
-    private ArrayList<ItemStack> findDyes(CraftingContainer inv, int plankRow){
-        ArrayList<ItemStack> dyes = new ArrayList<>();
+    private byte[] parseBasicColors(CraftingContainer inv) {
+        int plankRow = -1;
+        int plankCount = 0;
+        int leftmostPlankColumn = inv.getWidth();
+        int rightmostPlankColumn = -1;
+
         for(int i = 0; i < inv.getHeight(); ++i) {
-            if(i == plankRow){
-                continue;
-            }
             for(int j = 0; j < inv.getWidth(); ++j) {
                 int id = i*inv.getWidth() + j;
                 ItemStack stack = inv.getItem(id);
-                if(isDye(stack)){
-                    dyes.add(stack);
+                if (stack.isEmpty()) {
+                    continue;
                 }
-                else if(!stack.isEmpty()){
+                if (isPlank(stack)) {
+                    if (plankRow < 0) {
+                        plankRow = i;
+                    } else if (plankRow != i) {
+                        return null;
+                    }
+                    plankCount++;
+                    if (j < leftmostPlankColumn) {
+                        leftmostPlankColumn = j;
+                    }
+                    if (j > rightmostPlankColumn) {
+                        rightmostPlankColumn = j;
+                    }
+                } else if (!isDye(stack)) {
                     return null;
                 }
             }
         }
-        return dyes;
-    }
 
+        if (plankRow < 0 || plankCount != 3) {
+            return null;
+        }
+        if (rightmostPlankColumn - leftmostPlankColumn != 2) {
+            return null;
+        }
+
+        for(int j = 0; j < inv.getWidth(); ++j) {
+            int id = plankRow * inv.getWidth() + j;
+            ItemStack stack = inv.getItem(id);
+            if (!stack.isEmpty() && !isPlank(stack)) {
+                return null;
+            }
+        }
+
+        boolean hasDye = false;
+        byte[] basicColors = new byte[16];
+        for(int i = 0; i < inv.getHeight(); ++i) {
+            if (i == plankRow) {
+                continue;
+            }
+            for(int j = 0; j < inv.getWidth(); ++j) {
+                int id = i * inv.getWidth() + j;
+                ItemStack stack = inv.getItem(id);
+                if (stack.isEmpty()) {
+                    continue;
+                }
+                if (!isDye(stack)) {
+                    return null;
+                }
+                int colorIndex = getBasicColorIndex(stack);
+                if (colorIndex < 0) {
+                    return null;
+                }
+                if (basicColors[colorIndex] != 0) {
+                    return null;
+                }
+                basicColors[colorIndex] = 1;
+                hasDye = true;
+            }
+        }
+        return hasDye ? basicColors : null;
+    }
 
     /**
      * Used to check if a recipe matches current crafting inventory
      */
     @Override
     public boolean matches(CraftingContainer inv, Level worldIn) {
-        int plankRow = findPlankRow(inv);
-        if(plankRow < 0){
-            return false;
-        }
-        ArrayList<ItemStack> dyes = findDyes(inv, plankRow);
-        return dyes != null && !dyes.isEmpty();
+        return parseBasicColors(inv) != null;
     }
 
     /**
      * Returns an Item that is the result of this recipe
      */
     @Override
-    public ItemStack assemble(CraftingContainer inv, @NotNull RegistryAccess access) {
-        int plankRow = findPlankRow(inv);
-        if(plankRow < 0){
-            return ItemStack.EMPTY;
-        }
-        ArrayList<ItemStack> dyes = findDyes(inv, plankRow);
-        if(dyes == null || dyes.isEmpty()){
+    public ItemStack assemble(CraftingContainer inv, RegistryAccess access) {
+        byte[] basicColors = parseBasicColors(inv);
+        if (basicColors == null) {
             return ItemStack.EMPTY;
         }
 
-        byte[] basicColors = new byte[16];
-        for(ItemStack dye : dyes){
-            DyeColor color = ((DyeItem)(dye.getItem())).getDyeColor();
-            basicColors[15 - color.getId()] = 1;
-        }
         ItemStack result = new ItemStack(Items.ITEM_PALETTE.get());
         CompoundTag tag = result.getOrCreateTag();
         tag.putByteArray("basic", basicColors);
