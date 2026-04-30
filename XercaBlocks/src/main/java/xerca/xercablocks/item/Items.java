e86e9f444c634715b98b00ba777e0d6f1302aa52
ercan <xercax@gmail.com>
2026-04-30 19:28:04 +0300

    Placed terratiles in colored blocks tab

--- XercaBlocks/src/main/java/xerca/xercablocks/item/Items.java
+++ XercaBlocks/src/main/java/xerca/xercablocks/item/Items.java
@@ -20,6 +20,7 @@ public final class Items {
     public static final Item ROPE = new RopeItem(Blocks.ROPE, new Item.Properties());
     public static final Item CARVING_STATION = new BlockItem(Blocks.CARVING_STATION, new Item.Properties());
     private static final List<Item> BUILDING_TAB_ITEMS = new ArrayList<>();
+    private static final List<Item> COLORED_TAB_ITEMS = new ArrayList<>();
 
     private Items() {
     }
@@ -31,9 +32,9 @@ public final class Items {
         register("rope", ROPE);
         register("carving_station", CARVING_STATION);
         Blocks.carvedWoods().forEach(Items::registerBuildingBlockItem);
-        Blocks.terratiles().forEach(Items::registerBuildingBlockItem);
-        Blocks.terratileSlabs().forEach(Items::registerBuildingBlockItem);
-        Blocks.terratileStairs().forEach(Items::registerBuildingBlockItem);
+        Blocks.terratiles().forEach(Items::registerColoredBlockItem);
+        Blocks.terratileSlabs().forEach(Items::registerColoredBlockItem);
+        Blocks.terratileStairs().forEach(Items::registerColoredBlockItem);
 
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(entries -> {
             entries.accept(BOOKCASE);
@@ -45,12 +46,20 @@ public final class Items {
             entries.accept(BLOCK_STRAW);
             BUILDING_TAB_ITEMS.forEach(entries::accept);
         });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.COLORED_BLOCKS).register(entries ->
+                COLORED_TAB_ITEMS.forEach(entries::accept));
     }
 
     private static void registerBuildingBlockItem(String id, Block block) {
         Item item = new BlockItem(block, new Item.Properties());
         register(id, item);
         BUILDING_TAB_ITEMS.add(item);
+    }
+
+    private static void registerColoredBlockItem(String id, Block block) {
+        Item item = new BlockItem(block, new Item.Properties());
+        register(id, item);
+        COLORED_TAB_ITEMS.add(item);
     }
 
     private static void register(String id, Item item) {
